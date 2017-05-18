e4096e5339147592a48e819e186ffa93986b8f0a
Don Cooper <don_cooper@hotmail.com>
2017-05-18 15:07:17 -0400

    10 times done

--- roll_die_ten_times.rb
+++ roll_die_ten_times.rb
@@ -1,5 +1,7 @@
 # Roll the Die Ten Times
 #-----------------------
 
-roll1 = Random.rand(1..7)
-p "The result of your roll is #{roll1}"
+10.times do
+  roll1 = Random.rand(1..7)
+  p "The result of your roll is #{roll1}"
+end
