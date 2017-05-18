b170a24f2a45b4e1142a65b79263a6883372b8e4
Don Cooper <don_cooper@hotmail.com>
2017-05-18 15:03:53 -0400

    First commit

---
+++ roll_die.rb
@@ -0,0 +1,5 @@
+# Roll the Die
+#-------------
+
+roll1 = Random.rand(1..7)
+p "The result of your roll is #{roll1}"
---
+++ roll_die_ten_times.rb
@@ -0,0 +1,5 @@
+# Roll the Die Ten Times
+#-----------------------
+
+roll1 = Random.rand(1..7)
+p "The result of your roll is #{roll1}"
