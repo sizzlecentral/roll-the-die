e5d2252d97fcccc9d147d5c573dcb43c7bf47cf5
Don Cooper <don_cooper@hotmail.com>
2017-05-18 16:07:48 -0400

    Done lowest and highest

---
+++ lowest_and_highest.rb
@@ -0,0 +1,15 @@
+# Rolls Sorted
+#-------------
+
+roll_results =[]
+5.times do |roll|
+  roll = Random.rand(1..7)
+  roll_result = "The result of your roll is #{roll}"
+  roll_results << roll_result
+end
+
+puts roll_results.sort!
+lowest = roll_results.first
+highest = roll_results.last
+puts "The lowest number is #{lowest.slice(27)}."
+puts "The highest number is #{highest.slice(27)}."
